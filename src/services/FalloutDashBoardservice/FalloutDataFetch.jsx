d4dc861aeb01989208c99d3578fbf5d8cf90ed6c
Soam <Atul.Soam@lumen.com>
2025-06-05 19:03:06 +0530

    reverse commit

--- src/services/FalloutDashBoardservice/FalloutDataFetch.jsx
+++ src/services/FalloutDashBoardservice/FalloutDataFetch.jsx
@@ -2,6 +2,8 @@
 import axios from 'axios';
 
 const API_BASE = 'http://localhost:5000/api';
+// const API_BASE = 'https://asat-order-resolution-platform-ms-test1.rke-odc-test.corp.intranet/api';
+
 
 export const fetchFalloutData = async ({ page = 1, pageSize = 100, filters = {} }) => {
   try {
