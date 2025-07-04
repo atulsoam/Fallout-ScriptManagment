4a01cd280a52fb8b5d7f8563866f10f5284f8634
Atul Soam <atul.soam@hcltech.com>
2025-07-04 17:17:43 +0530

    Stop tracking config.py

--- Backend/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'SecerateKeyForYourApp'
-    MONGO_URI = 'mongodb+srv://atulsoam37:SMct1qp4ZyjFwAyw@cluster0.sjdvopm.mongodb.net/PROD_BM_ANALYTICS?retryWrites=true&w=majority&appName=Cluster0'
-    # SESSION_TYPE = 'filesystem'
